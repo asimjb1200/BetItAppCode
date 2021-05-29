7f57c873a3a851138f70bf1d8ad55ca556d0d5c4
Asim <asimjbrown@gmail.com>
2021-05-29 13:29:24 -0500

    Initial Commit

---
+++ BetIt/BetItApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetItApp.swift
+//  BetIt
+//
+//  Created by Asim Brown on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct BetItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
