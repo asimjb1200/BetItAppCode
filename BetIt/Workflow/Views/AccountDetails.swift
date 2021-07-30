ef6573e3da4420e32524ece6348a2037a174be34
Asim <asimjbrown@gmail.com>
2021-07-29 19:04:26 -0500

    added an enum for the items in the account details view. created a list of options that the user will be able to choose from when viewing their details

--- BetIt/Workflow/Views/AccountDetails.swift
+++ BetIt/Workflow/Views/AccountDetails.swift
@@ -13,26 +13,65 @@ var signalAlert = PassthroughSubject<Void,Never>()
 
 struct AccountDetails: View {
     @EnvironmentObject var user: UserModel
-    @State private var showingCustomSheet = false
-    
+//    @State private var showingCustomSheet = false
+    var accountOptions = ["Email", "Username", "Password", "Support", "Deactivate", "Log Out",]
+    @State private var email: String = ""
+    @State private var username: String = ""
+    @State private var password: String = ""
     var body: some View {
-                Button(action: {
-                    UserNetworking().logout(accessToken: user.accessToken, completion: { loggedOutStatus in
-                        switch loggedOutStatus {
-                        case .success(let isLoggedOut):
-                            DispatchQueue.main.async {
-                                user.logUserOut()
-                            }
-                        case .failure(let err):
-                            print(err)
-                        }
+        NavigationView {
+            List {
+                ForEach(accountOptions, id: \.self) { item in
+                    switch item {
+                    case AccountDeets.email.rawValue:
+                        NavigationLink("\(item)", destination: TextField("Change Email: ", text: $email))
+                    case AccountDeets.username.rawValue:
+                        NavigationLink("\(item)", destination: TextField("Change Username: ", text: $username))
+                    case AccountDeets.password.rawValue:
+                        NavigationLink("\(item)", destination: TextField("Change Password: ", text: $password))
+                    case AccountDeets.support.rawValue:
+                        NavigationLink("\(item)", destination: Text("Contact Support"))
+                    case AccountDeets.deactivate.rawValue:
+                        NavigationLink("\(item)", destination: Text("Deactivate Account"))
+                    default:
+                        Button(action: {
+                            UserNetworking().logout(accessToken: user.accessToken, completion: { loggedOutStatus in
+                                switch loggedOutStatus {
+                                case .success( _):
+                                    DispatchQueue.main.async {
+                                        user.logUserOut()
+                                    }
+                                case .failure(let err):
+                                    print(err)
+                                }
         
-                    })
-                }, label: {
-                    Text("Log Out")
-                })
+                            })
+                        }, label: {
+                            Text("Log Out")
+                        })
+                    }
+                    
+                }
+            }.navigationTitle("Account Details")
+        }
     }
 }
+
+//                Button(action: {
+//                    UserNetworking().logout(accessToken: user.accessToken, completion: { loggedOutStatus in
+//                        switch loggedOutStatus {
+//                        case .success( _):
+//                            DispatchQueue.main.async {
+//                                user.logUserOut()
+//                            }
+//                        case .failure(let err):
+//                            print(err)
+//                        }
+//
+//                    })
+//                }, label: {
+//                    Text("Log Out")
+//                })
 
 
 struct AccountDetails_Previews: PreviewProvider {
